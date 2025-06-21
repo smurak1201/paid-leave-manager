b86a5dfb485f1311f58acbc1095fc42ea0cf84e2
smurak1201 <s.murakoshi1201@gmail.com>
2025-06-21 16:37:05 +0900

    EmployeeModal コンポーネントのスタイルを改善し、アイコンを追加

--- src/components/employee/EmployeeModal.tsx
+++ src/components/employee/EmployeeModal.tsx
@@ -8,7 +8,8 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/modal";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Button, Input, Stack } from "@chakra-ui/react";
+import { Button, Input, Stack, HStack, Icon } from "@chakra-ui/react";
+import { User, BadgeInfo, CalendarPlus, CalendarCheck } from "lucide-react";
 import type { Employee } from "./types";
 
 interface Props {
@@ -30,60 +31,107 @@ export const EmployeeModal = ({
     <ModalOverlay />
     <ModalContent
       bg="white"
-      boxShadow="xl"
+      boxShadow="2xl"
       maxW="400px"
       w="90%"
       margin="auto"
       border="2px solid"
-      borderColor="teal.300"
+      borderColor="teal.400"
+      borderRadius="xl"
     >
-      <ModalHeader>従業員追加</ModalHeader>
-      <ModalCloseButton />
+      <ModalHeader textAlign="center" color="teal.700" fontWeight="bold">
+        <HStack justify="center" gap={2}>
+          <Icon as={User} color="teal.400" boxSize={6} />
+          <span>従業員追加</span>
+        </HStack>
+      </ModalHeader>
+      <ModalCloseButton color="teal.400" />
       <ModalBody>
         <Stack gap={3}>
           <FormControl isRequired>
-            <FormLabel>従業員コード</FormLabel>
-            <Input name="id" value={form.id} onChange={onChange} />
+            <FormLabel>
+              <Icon as={BadgeInfo} mr={2} />
+              従業員コード
+            </FormLabel>
+            <Input
+              name="id"
+              value={form.id}
+              onChange={onChange}
+              borderColor="teal.400"
+            />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel>姓</FormLabel>
-            <Input name="lastName" value={form.lastName} onChange={onChange} />
+            <FormLabel>
+              <Icon as={User} mr={2} />姓
+            </FormLabel>
+            <Input
+              name="lastName"
+              value={form.lastName}
+              onChange={onChange}
+              borderColor="teal.400"
+            />
           </FormControl>
           <FormControl isRequired>
-            <FormLabel>名</FormLabel>
+            <FormLabel>
+              <Icon as={User} mr={2} />名
+            </FormLabel>
             <Input
               name="firstName"
               value={form.firstName}
               onChange={onChange}
+              borderColor="teal.400"
             />
           </FormControl>
           <FormControl>
-            <FormLabel>付与日数</FormLabel>
+            <FormLabel>
+              <Icon as={CalendarPlus} mr={2} />
+              付与日数
+            </FormLabel>
             <Input
               name="total"
               type="number"
               min={0}
               value={form.total}
               onChange={onChange}
+              borderColor="teal.400"
             />
           </FormControl>
           <FormControl>
-            <FormLabel>消化日数</FormLabel>
+            <FormLabel>
+              <Icon as={CalendarCheck} mr={2} />
+              消化日数
+            </FormLabel>
             <Input
               name="used"
               type="number"
               min={0}
               value={form.used}
               onChange={onChange}
+              borderColor="teal.400"
             />
           </FormControl>
         </Stack>
       </ModalBody>
       <ModalFooter>
-        <Button colorScheme="teal" mr={3} onClick={onAdd}>
+        <Button
+          colorScheme="teal"
+          mr={3}
+          onClick={onAdd}
+          borderRadius="full"
+          px={6}
+        >
+          <Icon as={User} mr={2} />
           追加
         </Button>
-        <Button onClick={onClose}>キャンセル</Button>
+        <Button
+          onClick={onClose}
+          variant="ghost"
+          colorScheme="teal"
+          borderRadius="full"
+          px={6}
+        >
+          キャンセル
+        </Button>
       </ModalFooter>
     </ModalContent>
   </Modal>
