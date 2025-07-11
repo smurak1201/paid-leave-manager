673f23e83cbe324d048074aa623f8085ccb1fc0f
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-11 20:13:45 +0900

    ベースURLを設定ファイルに追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  base: '/paid-leave-manager/',
   plugins: [react(), tsconfigPaths()],
   server: {
     proxy: {
